12f79bf5178b23f917c89a0fa712b12194722e93
Rene Staritzbichler <rene@staritzbichler.com>
2020-05-15 08:51:43 +0200

    new gmx_cp_mdp.py

---
+++ gmx_cp_mdp.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import sys
+
+filename = sys.argv[1]
+outname = sys.argv[2]
+
+with open( outname , 'w') as w:
+    with open( filename) as r:
+        for l in r:
+          if "tc_grps" in l:
+              w.write( "tc_grps             = PROTMEMBLIG SOL       ; Couple lipids and SOL seperatly\n" )
+          elif "tau_t" in l:
+              w.write( "tau_t               = 0.5 0.5            ; Time constant for temperature coupling\n")
+          elif "ref_t" in l:
+              w.write ( "ref_t               = 310 310            ; Desired temperature (K)\n" )
+          else:
+              w.write( l )
