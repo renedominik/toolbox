f893122706d50548bd7260cb248d64e6b37469da
Rene Staritzbichler <rene@staritzbichler.com>
2020-08-27 14:23:32 +0200

    up

--- pdb_chain_flip.py
+++ pdb_chain_flip.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 
 #######################################
@@ -12,31 +12,30 @@ import sys
 
 # pruefe laenge der liste der argumente wenn ungleich print
 if len( sys.argv ) < 4:
-	print "USAGE:", sys.argv[0], "PDB ORIGINAL_CHAIN_1 ALTER_TO_CHAIN_1 (...) "
+	print( "USAGE:", sys.argv[0], "PDB ORIGINAL_CHAIN_1 ALTER_TO_CHAIN_1 (...) ")
 	exit(1)
 
 
 chains = {}
-for i in range( 0, len(sys.argv[2:])/2):
-    chains[ sys.argv[2*i+2] ] = sys.argv[2*i+3]
+for i in range( 0, int(len(sys.argv[2:])/2) ):
+        chains[ sys.argv[2*i+2] ] = sys.argv[2*i+3]
 
 #print( chains)
 
         
 #change side chain
 with open( sys.argv[1] ) as f: 
-    # gehe durch zeilen	
-    for l in f:                
-        # filtern von zeilen, die mit "ATOM" anfangen
-        if l[:4] != "ATOM":
-                print l,
-                continue
-        c = l[21]
-        if c not in chains:
-                print l,
-        # schneide zw Pos und add
-        else:
-                l = l[:21] + chains[c] + l[22:] 
-	    
-	        # print Block
-	        print l, 
+        # gehe durch zeilen	
+        for l in f:                
+                # filtern von zeilen, die mit "ATOM" anfangen
+                if l[:4] != "ATOM" and l[:6] != "HETATM":
+                        print( l.strip())
+                        continue
+                c = l[21]
+                if c not in chains:
+                        print( l.strip())
+                # schneide zw Pos und add
+                else:
+                        l = l[:21] + chains[c] + l[22:] 
+	                # print Block
+                        print( l.strip()) 
