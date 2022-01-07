560ce7a4c71210a74dab5bbda01f684055969edd
René Staritzbichler <rene@staritzbichler.com>
2022-01-07 19:06:36 +0100

    new aln2fa

--- pdb_conservation.py
+++ pdb_conservation.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 
+##############################
+# author: Rene Staritzbichler
+# 10.01.21
+##############################
+
 from Bio import PDB
 from Bio import AlignIO
+from Bio import SeqUtils
 import sys
+
+
+if len( sys.argv) < 5:
+    print( 'script writes sequence conservation into temperature factor of output PDB')
+    print( "USAGE:", sys.argv[0], 'PDB CHAIN ALIGNMENT.clw ROW')
+    print( "\tALIGNMENT is Clustal formatted multiple sequence alignment")
+    print( '\tROW starts with 0')
+    print()
+    help(SeqUtils.IUPACData)
+    exit(1)
+
 
 pdb_file = sys.argv[1]
 chain = sys.argv[2]
@@ -21,11 +38,13 @@ if ali_id >= nr:
 
     
 vals = []
+seq = []
 for i in range( 0, len( alignment[ ali_id ] )):
     aa = alignment[ ali_id][i]
     if aa == '-':
         print( '-',end=' ' )
         continue
+    seq.append( aa )
     count = 0
     for j in range( 0, nr):
         if j == ali_id: continue
@@ -42,5 +61,12 @@ with open( pdb_file) as r:
         if "ATOM" != l[0:4] or l[21] != chain:
             print( l)
         else:
-            if previd != l[22: 
-            print( l[:61] + " ".format() + l[72:] )
+            new_Id = int(l[22:26])
+            if previd != new_Id:
+                previd = new_Id
+                eidi += 1
+            aa = SeqUtils.IUPACData.protein_letters_3to1[ l[ 17:20 ].title()]
+            if aa != seq[eidi]:
+                print( "ERROR: sequence in alignment and pdb do not match:", eidi, seq[eidi],aa)
+                exit(1)
+            print( l[:60] + "{:6.2f}".format( vals[eidi] ) + l[66:] )
